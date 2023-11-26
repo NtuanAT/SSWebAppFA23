c3f25ced77d51f1982a871378a24c71035333817
tintruong98 <tintruong194@gmail.com>
2023-11-26 17:15:57 +0700

    Fix GetAllServiceOrder()

--- WebAPI/Controllers/OrderController.cs
+++ WebAPI/Controllers/OrderController.cs
@@ -256,8 +256,8 @@ namespace WebAPI.Controllers
 				var orders = _orderRepository.GetAll()
 				.Where(order => _orderServiceRepository.GetList(oss => oss.OrderId == order.Id).Any())
 				.ToList();
-
-				foreach (var order in orders)
+                var orderViewModels = new List<OrderViewModel>();
+                foreach (var order in orders)
 				{
 					var orderViewModel = new OrderViewModel
 					{
@@ -269,15 +269,18 @@ namespace WebAPI.Controllers
 
 					// Get account
 					var account = _accountRepository.Get(x => x.Id == order.AccountId);
-					orderViewModel.Account = new AccountViewModel
+					if (account != null)
 					{
-						Id = account.Id,
-						Username = account.Username,
-						Password = account.Password,
-						DetailId = account.DetailId,
-						Status = account.Status,
-						Role = account.Role
-					};
+                        orderViewModel.Account = new AccountViewModel
+                        {
+                            Id = account.Id,
+                            Username = account.Username,
+                            Password = account.Password,
+                            DetailId = account.DetailId,
+                            Status = account.Status,
+                            Role = account.Role
+                        };
+                    }   
 
 					// Get services
 					var orderServicesList = _orderServiceRepository.GetList(x => x.OrderId == order.Id);
@@ -285,27 +288,31 @@ namespace WebAPI.Controllers
 					foreach (var orderService in orderServicesList)
 					{
 						var service = _serviceRepository.Get(x => x.Id == orderService.ServiceId);
-						orderViewModel.Services.Add(new OrderServiceViewModel
+						if (service != null)
 						{
-							OrderId = orderService.OrderId,
-							ServiceId = orderService.ServiceId,
-							Quantity = orderService.Quantity,
-							Message = orderService.Message,
-							Service = new ServiceViewModel
-							{
-								Id = service.Id,
-								Price = service.Price,
-								Type = service.Type,
-							}
-						});
+                            orderViewModel.Services.Add(new OrderServiceViewModel
+                            {
+                                OrderId = orderService.OrderId,
+                                ServiceId = orderService.ServiceId,
+                                Quantity = orderService.Quantity,
+                                Message = orderService.Message,
+                                Service = new ServiceViewModel
+                                {
+                                    Id = service.Id,
+                                    Price = service.Price,
+                                    Type = service.Type,
+                                }
+                            });
+                        }   
 					}
+					orderViewModels.Add(orderViewModel);
 				}	
 
 				return new JsonResult(new
 				{
 					status = true,
 					message = "Get all service orders success",
-					data = orders
+					data = orderViewModels
 				});
 			}
 			catch (Exception ex)
